683e78a23c89aabf02d8e4e6051a3744293e48b0
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2018-03-05 09:51:36 -0500

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,12 +29,22 @@ namespace U1CareersShipston
         {
             lblMainLabel.Content = "Brock University:" + Environment.NewLine + " Bsc Honours: Computer Science";
             txtInformation.Text = "--OUAC Code: BG" + System.Environment.NewLine + "--# of Students in University: 17,877" + System.Environment.NewLine + "--Competitive: No" + System.Environment.NewLine + "--Location: St. Catharines, Ontario." + System.Environment.NewLine + "--Residential cost: Minimum 9,020, Maximum 12,790";
+            btnFirstYearCourses.Visibility = Visibility.Hidden;
+            btnMoreCourses.Visibility = Visibility.Hidden;
+            btnCourseCont.Visibility = Visibility.Hidden;
+            btnCourseCont2.Visibility = Visibility.Hidden;
+            btnCourseCont3.Visibility = Visibility.Hidden;
         }
 
         private void btnEducation_Click(object sender, RoutedEventArgs e)
         {
             lblMainLabel.Content = "Education Requirements";
             txtInformation.Text = "--Prerequisites Required: -English (ENG4U)" + System.Environment.NewLine + "-Advanced Functions (MHF4U) (min.70%) or Calculus and Vectors (MCV4U) (min.70%)" + System.Environment.NewLine + "Recommended Courses: -Computer and Information Sciences (ICS4U)";
+            btnFirstYearCourses.Visibility = Visibility.Hidden;
+            btnMoreCourses.Visibility = Visibility.Hidden;
+            btnCourseCont.Visibility = Visibility.Hidden;
+            btnCourseCont2.Visibility = Visibility.Hidden;
+            btnCourseCont3.Visibility = Visibility.Hidden;
         }
 
         private void btncourses_Click(object sender, RoutedEventArgs e)
@@ -42,24 +52,77 @@ namespace U1CareersShipston
             lblMainLabel.Content = "Courses";
             txtInformation.Text = "";
             btnFirstYearCourses.Visibility = Visibility.Visible;
-            btnMoreCourses.Visibility = Visibility.Visible;
+            btnMoreCourses.Visibility = Visibility.Visible; 
+            btnCourseCont.Visibility = Visibility.Hidden;
+            btnCourseCont2.Visibility = Visibility.Hidden;
+            btnCourseCont3.Visibility = Visibility.Hidden;
+
         }
 
         private void btnCareer_Click(object sender, RoutedEventArgs e)
         {
-
+            lblMainLabel.Content = "Career Paths";
+            txtInformation.Text = "List of Career Possibilities" + System.Environment.NewLine +
+                "Programmer or systems analyst" + System.Environment.NewLine +
+                "Computer graphics developer" + System.Environment.NewLine +
+                "Database or network administrator" + System.Environment.NewLine +
+                "IT project manager or corporate trainer" + System.Environment.NewLine +
+                "Web designer / Internet specialist";
+            btnFirstYearCourses.Visibility = Visibility.Hidden;
+            btnMoreCourses.Visibility = Visibility.Hidden;
+            btnCourseCont.Visibility = Visibility.Hidden;
+            btnCourseCont2.Visibility = Visibility.Hidden;
+            btnCourseCont3.Visibility = Visibility.Hidden;
         }
 
         private void btnFirstYearCourses_Click(object sender, RoutedEventArgs e)
         {
             lblMainLabel.Content = "First Year Courses";
-            txtInformation.Text = "";
+            txtInformation.Text = "APCO 1P00 Intro to Media Computation" + System.Environment.NewLine +
+                "APCO 1P01 Fluency with Technology" + System.Environment.NewLine +
+                "APCO 1P50 Integrity and Literacy in the Information Age" + System.Environment.NewLine +
+                "APCO 1P93 Applied Programming"; 
+            btnFirstYearCourses.Visibility = Visibility.Hidden;
+            btnMoreCourses.Visibility = Visibility.Hidden;
+            btnCourseCont.Visibility = Visibility.Visible;
         }
 
         private void btnMoreCourses_Click(object sender, RoutedEventArgs e)
         {
-            lblMainLabel.Content = "Post First Year";
-            txtInformation.Text = "";
+            lblMainLabel.Content = "General Course Info";
+            txtInformation.Text = "Including internships and CO-OPs, there are a total of 64 courses available." + System.Environment.NewLine +
+                "The Brock Computer Science Program is a four year program.";
+            btnFirstYearCourses.Visibility = Visibility.Hidden;
+            btnMoreCourses.Visibility = Visibility.Hidden;
+        }
+
+        private void btnCourseCont_Click(object sender, RoutedEventArgs e)
+        {
+            lblMainLabel.Content = "First Year Courses";
+            txtInformation.Text = "COSC 1P02 Introduction to Computer Science" + System.Environment.NewLine +
+                "COSC 1P03 Data Structures and Abstraction" + System.Environment.NewLine +
+                "COSC 1P50 Integrity and Literacy in the Information Age" + System.Environment.NewLine;
+            btnCourseCont.Visibility = Visibility.Hidden;
+            btnCourseCont2.Visibility = Visibility.Visible;
+        }
+
+        private void btnCourseCont2_Click(object sender, RoutedEventArgs e)
+        {
+            txtInformation.Text = "MATH 1P01 Calculus I" + System.Environment.NewLine +
+                "MATH 1P02 Calculus II" + System.Environment.NewLine +
+                "MATH 1P05 Applied Calculus I" + System.Environment.NewLine +
+                "MATH 1P06 Applied Calculus II" + System.Environment.NewLine +
+                "MATH 1P66 Mathematics for Computer Science I" + System.Environment.NewLine +
+                "MATH 1P67 Mathematics for Computer Science II";
+            btnCourseCont2.Visibility = Visibility.Hidden;
+            btnCourseCont3.Visibility = Visibility.Visible;
+        }
+
+        private void btnCourseCont3_Click(object sender, RoutedEventArgs e)
+        {
+            txtInformation.Text = "MATH 1P97 Differential and Integral Methods" + System.Environment.NewLine +
+                "MATH 1P98 Basic Statistical Methods";
+            btnCourseCont3.Visibility = Visibility.Hidden;
         }
     }
 }
